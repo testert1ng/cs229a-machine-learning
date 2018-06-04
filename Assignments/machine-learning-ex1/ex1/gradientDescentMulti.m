9fe28ad14c019a91c973fb802b914d47eaf11e41
Junting Zhu <jzhu5232@uni.sydney.edu.au>
2018-06-04 23:23:45 +1000

    init commit

---
+++ Assignments/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -0,0 +1,35 @@
+function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCostMulti) and gradient here.
+    %
+    
+    X_tmp = [];
+    
+    for i = 1:size(X, 2)
+      X_tmp = [X_tmp; sum((X * theta - y) .* X(:, i))];
+    end
+    
+    theta = theta - alpha / m * X_tmp;
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCostMulti(X, y, theta);
+
+end
+
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# CS229A Machine Learning
+
+## 0x00 Course Overview
+
+This course is taught by [Andrew Ng][1] and available to the public on [Coursera.org][2].
+
+## 0x01 Repo Structure
+
+### Assignment
+
+Contains all the programming assignments writing in Octave or MAtlab.
+
+### Lecture Notes
+
+Contains all the lecture notes.
+
+### Notes
+
+Contains all learning notes learned in this course.
+
+[1]: https://www.coursera.org/instructor/andrewng
+[2]: https://www.coursera.org/learn/machine-learning/
